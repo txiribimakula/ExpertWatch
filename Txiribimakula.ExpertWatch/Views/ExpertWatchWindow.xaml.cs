72eabc2143d703de7357a75f9143f56e208336c8
Pablo Tobalina <pablotobalina@gmail.com>
2020-08-27 20:43:09 +0200

    No need to "preload" assemblies now that all of them are netframework

--- Txiribimakula.ExpertWatch/Views/ExpertWatchWindow.xaml.cs
+++ Txiribimakula.ExpertWatch/Views/ExpertWatchWindow.xaml.cs
@@ -1,14 +1,10 @@
 ﻿using System.Windows.Controls;
-using Txiribimakula.ExpertWatch.Geometries;
-using Txiribimakula.ExpertWatch.Drawing;
 
 namespace Txiribimakula.ExpertWatch
 {
     public partial class ExpertWatchWindow : UserControl
     {
         public ExpertWatchWindow() {
-            Point point = new Point(0,0);
-            IDrawable drawable = new DrawableSegment(point, point);
             InitializeComponent();
         }
     }
