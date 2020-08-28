df1b3298ceba19702d3f5aa6492e3b8fc38dfdd6
Pablo Tobalina <pablotobalina@gmail.com>
2020-08-29 00:22:44 +0200

    Segment to path converter.

---
+++ Txiribimakula.ExpertWatch/Converters/SegmentToPathConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using Txiribimakula.ExpertWatch.Geometries.Contracts;
+
+namespace Txiribimakula.ExpertWatch
+{
+    public class SegmentToPathConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+            ISegment segment = (ISegment)value;
+
+            return "M " + segment.InitialPoint.X.ToString(CultureInfo.InvariantCulture) + "," + segment.InitialPoint.Y.ToString(CultureInfo.InvariantCulture) +
+                " L" + segment.FinalPoint.X.ToString(CultureInfo.InvariantCulture) + "," + segment.FinalPoint.Y.ToString(CultureInfo.InvariantCulture);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+            return null;
+        }
+    }
+}
