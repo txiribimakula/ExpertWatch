f3c1fb98406c02f3c8f05b4bfd6db6da418d94e3
Pablo Tobalina <pablotobalina@gmail.com>
2020-09-10 09:36:21 +0200

    Show progress on description field.

---
+++ Txiribimakula.ExpertWatch/Converters/DrawablesToProgressConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using Txiribimakula.ExpertWatch.Drawing;
+using Txiribimakula.ExpertWatch.Geometries.Contracts;
+
+namespace Txiribimakula.ExpertWatch.Converters
+{
+    public class DrawablesToProgressConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+            DrawableCollection<IDrawable> drawables = (DrawableCollection<IDrawable>)value;
+
+            if(drawables.Count > 0) {
+                return 100;
+            } else {
+                return 0;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+            return null;
+        }
+    }
+}
