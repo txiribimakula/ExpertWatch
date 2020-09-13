b7185a3e887c631dba062aa7aa6935e22c0d7c11
Pablo Tobalina <pablotobalina@gmail.com>
2020-09-13 20:44:31 +0200

    Rename to ExpertDebug

--- Txiribimakula.ExpertWatch/Properties/AssemblyInfo.cs
+++ Txiribimakula.ExpertWatch/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Txiribimakula.ExpertWatch")]
+[assembly: AssemblyTitle("Txiribimakula.ExpertDebug")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Txiribimakula.ExpertWatch")]
+[assembly: AssemblyProduct("Txiribimakula.ExpertDebug")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
