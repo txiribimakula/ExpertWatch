30a2e60abfa693d95de69abe7dea004b755029cc
Pablo Tobalina <pablotobalina@gmail.com>
2020-09-20 18:20:30 +0200

    Fix path for arcs.

--- Txiribimakula.ExpertWatch/Converters/ArcToPathConverter.cs
+++ Txiribimakula.ExpertWatch/Converters/ArcToPathConverter.cs
@@ -11,8 +11,10 @@ namespace Txiribimakula.ExpertWatch
             IArc arc = (IArc)value;
             string path = "M" + arc.InitialPoint.X.ToString(CultureInfo.InvariantCulture) + "," + arc.InitialPoint.Y.ToString(CultureInfo.InvariantCulture) +
                 " A" + arc.Radius.ToString(CultureInfo.InvariantCulture) + "," + arc.Radius.ToString(CultureInfo.InvariantCulture) +
-                " " + arc.SweepAngle.ToString(CultureInfo.InvariantCulture) + " 0 1 " +
-                arc.FinalPoint.X.ToString(CultureInfo.InvariantCulture) + "," + arc.FinalPoint.Y.ToString(CultureInfo.InvariantCulture);
+                " " + arc.SweepAngle.ToString(CultureInfo.InvariantCulture) +
+                " " + (arc.SweepAngle >= 180 ? "1" : "0") +
+                " " + (arc.SweepAngle > 0 ? "0" : "1") +
+                " " + arc.FinalPoint.X.ToString(CultureInfo.InvariantCulture) + "," + arc.FinalPoint.Y.ToString(CultureInfo.InvariantCulture);
             return path;
 
         }
